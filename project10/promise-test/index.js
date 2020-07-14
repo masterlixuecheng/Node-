b138a4e66400d513eec85ec1bd96599bd804c2a4
李雪铖 <1097987619@qq.com>
2020-07-14 11:08:02 +0800

    下了老师的源码，开始学习koa2

---
+++ project10/promise-test/index.js
@@ -0,0 +1,104 @@
+const fs = require('fs')
+const path = require('path')
+
+// // callback 方式获取一个文件的内容
+// function getFileContent(fileName, callback) {
+//     const fullFileName = path.resolve(__dirname, 'files', fileName)
+//     fs.readFile(fullFileName, (err, data) => {
+//         if (err) {
+//             console.error(err)
+//             return
+//         }
+//         callback(
+//             JSON.parse(data.toString())
+//         )
+//     })
+// }
+
+// // 测试 callback-hell
+// getFileContent('a.json', aData => {
+//     console.log('a data', aData)
+//     getFileContent(aData.next, bData => {
+//         console.log('b data', bData)
+//         getFileContent(bData.next, cData => {
+//             console.log('c data', cData)
+//         })
+//     })
+// })
+
+// 用 promise 获取文件内容
+function getFileContent(fileName) {
+    const promise = new Promise((resolve, reject) => {
+        const fullFileName = path.resolve(__dirname, 'files', fileName)
+        fs.readFile(fullFileName, (err, data) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(
+                JSON.parse(data.toString())
+            )
+        })
+    })
+    return promise
+}
+
+// getFileContent('a.json').then(aData => {
+//     console.log('a data', aData)
+//     return getFileContent(aData.next)
+// }).then(bData => {
+//     console.log('b data', bData)
+//     return getFileContent(bData.next)
+// }).then(cData => {
+//     console.log('c data', cData)
+// })
+
+async function readFileData() {
+    // 同步写法(其实是异步函数)
+    // await 外面必须有一个 async 函数包裹
+    // await 后面跟一个 promise 会把 promise resolve 出来的返回值，直接赋值给前面的值
+    try {
+        const aData = await getFileContent('a.json')
+        console.log('a data', aData)
+        const bData = await getFileContent(aData.next)
+        console.log('b data', bData)
+        const cData = await getFileContent(bData.next)
+        console.log('c data', cData)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+readFileData()
+
+
+async function readAData() {
+    const aData = await getFileContent('a.json')
+    return aData
+}
+async function test() {
+    const aData = await readAData()
+    console.log(aData)
+}
+test()
+
+async function readBData() {
+    const bData = await getFileContent('a.json')
+    return bData
+}
+async function testB() {
+    const bData = await readBData()
+    console.log(bData)
+}
+testB()
+// a data { next: 'b.json', msg: 'this is a' }
+// { next: 'b.json', msg: 'this is a' }
+// { next: 'b.json', msg: 'this is a' }
+// b data { next: 'c.json', msg: 'this is b' }
+// c data { next: null, msg: 'this is c' }
+
+// async await 要点：
+// 1. await 后面可以追加 promise 对象，获取 resolve 的值
+// 2. await 必须包裹在 async 函数里面
+// 3. async 函数执行返回的也是一个 promise 对象
+// 4. try-catch 截获 promise 中 reject 的值
